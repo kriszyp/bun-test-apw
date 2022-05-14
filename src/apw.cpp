707cb53e6c4061774f899d256e36bde8ebc1ac4b
Kris Zyp <kriszyp@gmail.com>
2022-05-13 21:04:38 -0600

    More helpful information for debugging/testing

--- src/apw.cpp
+++ src/apw.cpp
@@ -17,9 +17,11 @@ class TestAPW : public AsyncProgressWorker<uint32_t> {
             // Need to simulate cpu heavy task
             // Note: This Send() call is not guaranteed to trigger an equal
             // number of OnProgress calls (read documentation above for more info)
+            fprintf(stderr, "start executing\n");
             for (uint32_t i = 0; i < 1000; ++i) {
               progress.Send(&i, 1);
             }
+            fprintf(stderr, "done executing\n");
         }
         
         void OnError(const Error &e) {
@@ -29,15 +31,19 @@ class TestAPW : public AsyncProgressWorker<uint32_t> {
         }
 
         void OnOK() {
-            HandleScope scope(Env());
-            // Pass no error, give back original data
+            fprintf(stderr, "OnOK\n");
+            // return -1
             Callback().Call({Env().Null(), Number::New(Env(), -1)});
         }
 
         void OnProgress(const uint32_t* data, size_t /* count */) {
-            HandleScope scope(Env());
-            // Pass no error, no echo data, but do pass on the progress data
-            Callback().Call({Env().Null(), Number::New(Env(), *data)});
+            fprintf(stderr, "OnProgress\n");
+
+            // this does a callback while avoiding the escapable scopes
+            napi_value result;
+            napi_call_function(Env(), Env().Undefined(), Callback().Value(), 0, {}, &result);
+            // this does a callback, but I think it uses escapable scopes
+            //Callback().Call({Env().Null(), Number::New(Env(), *data)});
         }
 
 };
@@ -45,9 +51,11 @@ napi_value start(napi_env env, napi_callback_info info) {
 	size_t argc = 1;
 	napi_value args[1];
 	napi_get_cb_info(env, info, &argc, args, NULL, NULL);
-
-	TestAPW* worker = new TestAPW(Function(env, args[0]));
+    Function callback = Function(env, args[0]);
+    fprintf(stderr, "created\n");
+	TestAPW* worker = new TestAPW(callback);
 	worker->Queue();
+    fprintf(stderr, "queued\n");
 	napi_value result;
 	napi_get_undefined(env, &result);
 	return result;
@@ -59,8 +67,10 @@ napi_value Init(napi_env env, napi_value exports) {
 	return exports;
 }
 
+
 NAPI_MODULE_INIT() {
 	EXPORT_NAPI_FUNCTION("start", start);
 	return exports;
 }
+// According to the docs, this is another valid way to register a NAPI module, but this fails with:  TypeError: symbol 'napi_register_module_v1' not found in native module. Is this a Node API (napi) module?
 //NAPI_MODULE(extractor, Init)
